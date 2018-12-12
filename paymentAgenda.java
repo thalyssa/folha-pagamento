57285da9d6e9d712d3d300affef9055c00456186
Yami Thalyssa <thalyssa2.0@hotmail.com>
2018-12-12 12:19:04 -0300

    Create paymentAgenda.java

---
+++ paymentAgenda.java
@@ -0,0 +1,15 @@
+public class paymentAgenda {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public paymentAgenda(String description){
+        this.description = description;
+    }
+}
