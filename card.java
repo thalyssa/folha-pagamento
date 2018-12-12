a3408cb8efcc1fb218d37f9bc9d94d55677c595f
Yami Thalyssa <thalyssa2.0@hotmail.com>
2018-12-12 05:04:36 -0300

    Update card.java

--- card.java
+++ card.java
@@ -2,59 +2,55 @@ import java.lang.Float;
 
 public class card{
 
-	private String entry;
-	private String exit;
-	private int func_code;
-	private float hours;
+    private String entry;
+    private String exit;
+    private int func_code;
+    private float hours;
 
-	public void setCode(int code){
-		this.func_code = code;
-	}
+    public String getEntry(){
+        return this.entry;
+    }
 
-	public String getEntry(){
-		return this.entry;
-	}
+    public void setEntry(String entry){
+        this.entry = entry;
+    }
 
-	public void setEntry(String entry){
-		this.entry = entry;
-	}
+    public String getExit(){
+        return this.exit;
+    }
 
-	public String getExit(){
-		return this.exit;
-	}
+    public void setExit(String exit){
+        this.exit = exit;
+    }
 
-	public void setExit(String exit){
-		this.exit = exit;
-	}
+    public float getHours(){
+        return this.hours;
+    }
 
-	public float calcHours(String entry, String exit){
-		String[] entryTime = entry.split(":");
-		String[] exitTime = exit.split(":");
+    public float calcHours(String entry, String exit){
+        String[] entryTime = entry.split(":");
+        String[] exitTime = exit.split(":");
 
-		float entH = Float.parseFloat(entryTime[0]); //Hora de entrada
-		float entM = Float.parseFloat(entryTime[1]); //Minutos da entrada
+        float entH = Float.parseFloat(entryTime[0]); //Hora de entrada
+        float entM = Float.parseFloat(entryTime[1]); //Minutos da entrada
 
-		float exH = Float.parseFloat(exitTime[0]);
-		float exM = Float.parseFloat(exitTime[1]);
-		
-		entH += (entM/100);
-		exH += (exM/100);
+        float exH = Float.parseFloat(exitTime[0]);
+        float exM = Float.parseFloat(exitTime[1]);
 
-		float workHours = entH - exH;
+        entH += (entM/100);
+        exH += (exM/100);
 
-		return workHours;
-	}
+        float workHours = exH - entH;
 
-	public void setHours(float hours){
-		this.hours = hours;
-	}
+        return workHours;
+    }
 
-	public card(String entry, String exit, int code){
-		this.entry = entry;
-		this.exit = exit;
-		this.func_code = code;
-		this.hours = this.calcHours(entry, exit);
-	}
+    public card(String entry, String exit, int code){
+        this.entry = entry;
+        this.exit = exit;
+        this.func_code = code;
+        this.hours = this.calcHours(entry, exit);
+    }
 
 
 }
